f653ec80dea37a24907b11a8746858de59cd01d2
TBA5854 <adhavan5030@gmail.com>
2023-07-24 12:48:03 +0530

    Finally Done . SNL is completer

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import pygame as pg
 from time import sleep
 from random import randint
-choice=0#int(input("Do you want to play 2 player or player vs computer : (0 for computer and 1 for 2 player)\n>>"))
+choice=int(input("Do you want to play 2 player or player vs computer : (0 for computer and 1 for 2 player)\n>>"))
 while choice not in [0,1]:
-    print("INVALID INPUT \n")
+    print("\nINVALID INPUT!!!\n")
     choice=int(input("Do you want to play 2 player or player vs computer : (0 for computer and 1 for 2 player)\n>>"))
 if choice:
     name_1=input("Enter Player 1 Name \n>>")
     name_2=input("Enter Player 2 Name \n>>")
 else:
-    name_1="TBA"#input("Enter Player 1 Name \n>>")
+    name_1=input("Enter Player 1 Name \n>>")
     name_2="Computer"
 ladder={4:25,21:39,29:74,43:76,63:80,71:89}
 snake={30:7,47:15,56:19,73:51,82:42,92:75,98:55}
@@ -31,9 +31,6 @@ class img_loader:
         window.blit(title,(230,50))
         whose_turn=self.font_40.render("TURN : "+turn_player, True, (150,60,150))
         window.blit(whose_turn,(290,520))
-        self.button_rect=pg.draw.rect(window ,button_color, (button_x, button_y, button_width, button_height))
-        roll_button=self.font_36.render("ROLL", True, (0,0,0))
-        window.blit(roll_button,(750,178))
     font_40 = pg.font.Font(None,40)
     font_36 = pg.font.Font(None, 36)
     font_70 = pg.font.Font(None, 70)
@@ -74,16 +71,16 @@ class player:
                 pg.display.flip()
         self.square_no=self.target_square_no
     def move_multiple(self,steps,player_2,img):
-        for i in range(steps-1):
+        for i in range(steps):
             if self.square_no==100:
                 break
-            self.move(self.square_no+1,player_2,pg_img_loader)
+            self.move(self.square_no+1,player_2,img)
         if ladder.get(self.square_no,0) or snake.get(self.square_no,0):
             if ladder.get(self.square_no,0):
                 self.target_square_no=ladder.get(self.square_no,0)-1
                 self.move(self.target_square_no+1,player_2,pg_img_loader,1)
             else:
-                self.target_square_no=snake.get(self.square_no,0)-1
+                self.target_square_no=snake.get(self.square_no,0)
                 self.move(self.target_square_no,player_2,pg_img_loader,1)
 def win():
     pg.draw.rect(window ,(205,205,205), (0, 120, 1000,400))
@@ -113,60 +110,68 @@ class dice:
             roll_text=pg_img_loader.font_36.render("   DICE", True, (50,50,50))
             window.blit(roll_text,(730,325))
             pg.display.flip()
-            #sleep(0.5)
+            sleep(0.5)
         del self.i
         return self.roll(src)
     def roll(self,src):
         self.roll_no=randint(0,5)
         for self.i in range (3):
+            self.x=725
+            self.y=355
+            window.blit(self.dice_tuple[self.roll_no],(self.x,self.y))
+            roll_text=pg_img_loader.font_36.render("  DICE", True, (50,50,50))
+            window.blit(roll_text,(730,325))
+            pg.display.flip()
+            sleep(1)
             src.load()
             p1.img_load()
             p2.img_load()
+            roll_text=pg_img_loader.font_36.render("  DICE", True, (50,50,50))
+            window.blit(roll_text,(730,325))
             pg.display.flip()
-            #sleep(1)
-            self.x=20
-            self.y=20
-            window.blit(self.dice_tuple[self.roll_no],(self.x,self.y))
-            pg.display.flip()
-            #sleep(1)
+            sleep(1)
         del self.i
         return self.roll_no+1
 class interchange_2_player:
     turn=1
     def swapper(self,p1,p2):
+        global turn_player
         if self.turn:
-            p1.move_multiple(dicee.rolling(pg_img_loader)+1,p2,pg_img_loader)
+            p1.move_multiple(dicee.rolling(pg_img_loader),p2,pg_img_loader)
             self.turn-=1
             turn_player=p2.name
         else:
-            p2.move_multiple(dicee.rolling(pg_img_loader)+1,p1,pg_img_loader)
+            p2.move_multiple(dicee.rolling(pg_img_loader),p1,pg_img_loader)
             self.turn+=1
             turn_player=p1.name
-        if p1.square_no>p2.square_no:
-            pg_img_loader.color=[180,180,247]
-        elif p1.square_no<p2.square_no:
-            pg_img_loader.color=[247,180,180]
-        elif p1.square_no==p2.square_no:
-            pg_img_loader.color=[180,180,180]
+        color_check()
 class interchange_computer:
     def swapper(self,p1,p2):
-        if p1.square_no>p2.square_no:
-            pg_img_loader.color=[180,180,247]
-        elif p1.square_no<p2.square_no:
-            pg_img_loader.color=[247,180,180]
-        elif p1.square_no==p2.square_no:
-            pg_img_loader.color=[180,180,180]
+        global turn_player
         p1.move_multiple(dicee.rolling(pg_img_loader),p2,pg_img_loader)
-        print(dicee.roll_no)
+        turn_player=p2.name
+        color_check()
+        if(p1.square_no>=100):
+            return
         sleep(2)
-        if p1.square_no>p2.square_no:
-            pg_img_loader.color=[180,180,247]
-        elif p1.square_no<p2.square_no:
-            pg_img_loader.color=[247,180,180]
-        elif p1.square_no==p2.square_no:
-            pg_img_loader.color=[180,180,180]
-        p2.move_multiple(dicee.rolling(pg_img_loader)+1,p1,pg_img_loader)
-        print(dicee.roll_no)
+        p2.move_multiple(dicee.rolling(pg_img_loader),p1,pg_img_loader)
+        turn_player=p1.name
+        color_check()
+        if(p2.square_no>=100):
+            return
+def color_check ():
+    if p1.square_no>p2.square_no:
+        pg_img_loader.color=[180,180,247]
+    elif p1.square_no<p2.square_no:
+        pg_img_loader.color=[247,180,180]
+    elif p1.square_no==p2.square_no:
+        pg_img_loader.color=[180,180,180]
+    if p1.square_no>=100 or p2.square_no>=100:
+        pg_img_loader.color=[180,247,180]
+    pg_img_loader.load()
+    p1.img_load()
+    p2.img_load()
+    pg.display.flip()
 if choice:
     player_change=interchange_2_player()
 else:
@@ -197,25 +202,26 @@ while running:
             running=False
     if(p1.square_no>=100):
         win()
-        #sleep(1)
+        sleep(1)
         continue
     if(p2.square_no>=100):
         win()
-        #sleep(1)
+        sleep(1)
         continue
     pg_img_loader.load()
     p1.img_load()
     p2.img_load()
+    button_rect=pg.draw.rect(window ,button_color, (button_x, button_y, button_width, button_height))
+    roll_button=pg_img_loader.font_36.render("ROLL", True, (0,0,0))
+    window.blit(roll_button,(750,178))
     pg.display.flip()
     if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-        if pg_img_loader.button_rect.collidepoint(event.pos) and not is_clicked:
+        if button_rect.collidepoint(event.pos) and not is_clicked:
             is_clicked = True
             if is_both_players_on_same_square:
                 p2.pos_y-=2
                 p1.pos_y+=2
                 is_both_players_on_same_square=0
-            print("Button clicked!")
-            print(p1.pos_x,p1.pos_y,p1.square_no)
             player_change.swapper(p1,p2)
             if p1.square_no==p2.square_no:
                 is_both_players_on_same_square=1
@@ -226,10 +232,4 @@ while running:
         pass
     clockk.tick(fps)
 pg.quit()
-# To undo 
-# sleep\
-# prob:
-# roll not consistent
-# turn name nit changing
-# display should update as soon as either players tutn ends and update color instead of updating after some action by user
-# roll button should not appear when other computer playing or animating time 
+print("\t\tThanks for playing\n\n\t\tA Program by TBA5854")
